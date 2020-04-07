f3871317faad199ba71fe3e11782111e999f3368
XieFucai <1977596495@qq.com>
2020-04-07 22:01:50 +0800

    测试DI

--- src/com/asu/test/TestIOC.java
+++ src/com/asu/test/TestIOC.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.asu.entity.Student;
+import com.asu.entity.User;
 
 /**
  * @author Shieh
@@ -15,19 +16,22 @@ public class TestIOC {
 
 	public static void main(String[] args) {
 		// 1、加载配置文件，构建spring容器；
-//		Resource resource = new ClassPathXmlApplicationContext().getResource("spring-config.xml");
 		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-config.xml");
 		// 2、从容器中获得相应的对象；
-		// Student student = new Student();//正常情况下
-		// student.setSname("老谢");
-		Student student = (Student) ac.getBean("stu");
-		// 3、调用相应方法进行测试。
-		student.show();
+//		Student student = (Student) ac.getBean("stu");
+//		// 3、调用相应方法进行测试。
+//		student.show();
 
-		// IOC创建对象-通过构造器测试
 		Student student1 = (Student) ac.getBean("stu1");
-//		Student student1 = (Student) ac.getBean(Student.class);
 		student1.show();
+
+		// p命令空间注入
+		User user = (User) ac.getBean("u1");
+		user.show();
+
+		// c命令空间注入
+		User user1 = (User) ac.getBean("u2");
+		user1.show();
 	}
 
 }
